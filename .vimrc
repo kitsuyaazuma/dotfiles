101c35b6c041f92c463e5ba685bb93ae03087c09
Kitsuya0828 <kitsuyaazuma@gmail.com>
2024-03-26 22:54:04 +0900

    feat: monokai color scheme

---
+++ .vimrc
@@ -0,0 +1,4 @@
+"https://github.com/ku1ik/vim-monokai
+syntax enable
+colorscheme monokai
+
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://github.com/ku1ik/vim-monokai
+mkdir -p ~/.vim/colors
+wget https://raw.githubusercontent.com/ku1ik/vim-monokai/master/colors/monokai.vim -P ~/.vim/colors/
