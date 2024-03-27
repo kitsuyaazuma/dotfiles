7a745bbcd0befcb724339142ae2360a73f871fbe
Kitsuya0828 <kitsuyaazuma@gmail.com>
2024-03-27 23:59:18 +0900

    feat: vim-plugin & coc.nvim

--- .vimrc
+++ .vimrc
@@ -2,3 +2,10 @@
 syntax enable
 colorscheme monokai
 
+call plug#begin()
+
+" https://github.com/neoclide/coc.nvim
+Plug 'neoclide/coc.nvim', { 'branch': 'release' }
+
+call plug#end()
+
--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# https://github.com/junegunn/vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # https://github.com/ku1ik/vim-monokai
 mkdir -p ~/.vim/colors
 wget https://raw.githubusercontent.com/ku1ik/vim-monokai/master/colors/monokai.vim -P ~/.vim/colors/
