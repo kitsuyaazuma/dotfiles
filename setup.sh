1603dbfbff045ed1b3ccdcadfba1558a3c621500
Kitsuya0828 <kitsuyaazuma@gmail.com>
2024-04-11 00:39:54 +0900

    feat: github copilot

--- setup.sh
+++ setup.sh
@@ -7,3 +7,8 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 # https://github.com/ku1ik/vim-monokai
 mkdir -p ~/.vim/colors
 wget https://raw.githubusercontent.com/ku1ik/vim-monokai/master/colors/monokai.vim -P ~/.vim/colors/
+
+# https://github.com/github/copilot.vim
+git clone https://github.com/github/copilot.vim.git \
+  ~/.vim/pack/github/start/copilot.vim
+# :Copilot setup
