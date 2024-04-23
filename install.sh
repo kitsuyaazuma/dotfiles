97535200a6de13350fa8176935dd8147747ac770
Kitsuya0828 <kitsuyaazuma@gmail.com>
2024-04-23 11:51:58 +0900

    fix: check if file exists

--- install.sh
+++ install.sh
@@ -12,15 +12,28 @@ for f in .??*; do
 done
 
 # https://github.com/junegunn/vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ -f ~/.vim/autoload/plug.vim ]; then
+    echo "vim-plug already installed"
+else
+    echo "install vim-plug"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # https://github.com/ku1ik/vim-monokai
 mkdir -p ~/.vim/colors
-wget https://raw.githubusercontent.com/ku1ik/vim-monokai/master/colors/monokai.vim -P ~/.vim/colors/
+if [ -f ~/.vim/colors/monokai.vim ]; then
+    echo "vim-monokai already installed"
+else
+    echo "install vim-monokai"
+    wget https://raw.githubusercontent.com/ku1ik/vim-monokai/master/colors/monokai.vim -P ~/.vim/colors/
+fi
 
 # https://github.com/github/copilot.vim
-git clone https://github.com/github/copilot.vim.git \
-  ~/.vim/pack/github/start/copilot.vim
-# :Copilot setup
+if [ -d ~/.vim/pack/github/start/copilot.vim ]; then
+    echo "copilot.vim already installed"
+else
+    echo "install copilot.vim"
+    git clone https://github.com/github/copilot.vim.git ~/.vim/pack/github/start/copilot.vim
+    #TODO :Copilot setup
+fi
 
