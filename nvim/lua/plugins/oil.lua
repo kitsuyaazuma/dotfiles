250d256b4709003c67b72012064132a3d7b1b783
kitsuya0828 <kitsuyaazuma@gmail.com>
2024-12-26 11:41:59 +0900

    feat: oil.nvim keymap

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,12 +2,10 @@ return {
 	'stevearc/oil.nvim',
 	---@module 'oil'
 	---@type oil.SetupOpts
-	opts = {},
-	-- Optional dependencies
-	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 
 	config = function()
-		require('oil').setup()
+		require("oil").setup()
+		vim.keymap.set('n', '<C-n>', require('oil').open)
 	end,
 }
