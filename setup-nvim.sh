ac16074411037df5fa40bed496fb2b10e04ffc3f
kitsuya0828 <kitsuyaazuma@gmail.com>
2025-02-16 22:37:56 +0900

    fix: install neovim as appimage

--- setup-nvim.sh
+++ setup-nvim.sh
@@ -17,13 +17,13 @@ if ! command -v nvim &> /dev/null; then
 	brew install neovim
 	;;
     Linux)
-	apt install neovim
-	# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
-	# chmod u+x nvim-linux-x86_64.appimage
-	# ./nvim-linux-x86_64.appimage --appimage-extract
-	# ./squashfs-root/AppRun --version
-	# sudo mv squashfs-root /
-	# sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+	# apt install neovim
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+	chmod u+x nvim-linux-x86_64.appimage
+	./nvim-linux-x86_64.appimage --appimage-extract
+	./squashfs-root/AppRun --version
+	sudo mv squashfs-root /
+	sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 	;;
     esac
 fi
