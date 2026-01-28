5330e8886022a44c558663b40d9639110d13c972
kitsuyaazuma <kitsuyaazuma@gmail.com>
2026-01-28 13:02:24 +0900

    feat: use background color identation

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,7 +1,39 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	---@module "ibl"
-	---@type ibl.config
-	opts = {},
+	opts = function()
+		local highlight = {
+			"CursorColumn",
+			"WhiteSpace",
+		}
+		return {
+			indent = {
+				highlight = highlight,
+				char = "",
+			},
+			whitespace = {
+				highlight = highlight,
+				remove_blankline_trail = true,
+			},
+			scope = { enabled = false },
+			exclude = {
+				filetypes = {
+					"Trouble",
+					"alpha",
+					"dashboard",
+					"help",
+					"lazy",
+					"mason",
+					"neo-tree",
+					"notify",
+					"snacks_dashboard",
+					"snacks_notif",
+					"snacks_terminal",
+					"snacks_win",
+					"toggleterm",
+					"trouble",
+				},
+			},
+		}
+	end,
 }
